7ca0b6487648daaa20c9bc9a2f49a7d397364544
benelgiz <benelgiz@gmail.com>
2017-03-14 15:46:07 +0100

    Modification and File addition
    - All data indexes corresponding the fault1 and fault2 durations are found.
    - Another flight data added.

--- dataRead.m
+++ dataRead.m
@@ -1,11 +1,13 @@
 % Written by Ewoud Smeur 
 % Modified by Elgiz Baskaya
 
+
+
 %%%%%%% This part from Ewoud %%%%%%%%%
-filename = 'flightExamp.data';
+filename = 'flightData2.data';
 
 
-formatSpec = '%f%f%s%f%s%f%f%f%f%f%f%f%f%f%f%f%f%f';
+formatSpec = '%f%f%s%f%s%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f';
 
 formatSpecHeader = '%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%[^\n\r]';
 delimiter = ' ,';
@@ -70,6 +72,7 @@ t_gyro = dataArray{1, 1}(gyro_id);
 
 commands_id_only = strcmp(dataArray{1,3},'COMMANDS');
 commands_id = drone_select_id & commands_id_only & flight_duration_id;
+commands_index = find(commands_id == 1);
 commands(:,1) = dataArray{1, 7}(commands_id);
 commands(:,2) = dataArray{1, 8}(commands_id);
 t_commands = dataArray{1, 1}(commands_id);
@@ -77,6 +80,58 @@ t_commands = dataArray{1, 1}(commands_id);
 gps_id = drone_select_id & gps_id & flight_duration_id;
 altitude(:,1) = dataArray{1, 8}(gps_id)/1000;
 t_altitude = dataArray{1, 1}(gps_id);
+
+% Labeling outputs (Fault, Normal)
+
+% FAULT 1
+% Finding the faulty command indexes
+index_negative1 = find(dataArray{1,7} < 0);
+negative1_id = zeros(length(dataArray{1,7}),1);
+% Sets indexes to 1s if the altitude is greater than the given limit
+negative1_id(index_negative1) = 1;
+% And with commands id to find negative values corresponding to COMMANDS 
+faultyCommand1_id = negative1_id & commands_id_only & flight_duration_id &drone_select_id;
+fault1_index = find(faultyCommand1_id==1);
+
+isFault1Command = ismember(commands_index,fault1_index);
+faulty1_id = diff(isFault1Command)~=0;
+changingIndexes1 = find(faulty1_id==1);
+groupedChangingIndexes1 = reshape(changingIndexes1,2,[]);
+% detecting change one index before
+[m,n] = size(groupedChangingIndexes1);
+groupedChangingIndexes1 = ones(m,n) + groupedChangingIndexes1;
+
+isFault1 = zeros(length(dataArray{1,1}),1);
+
+for i = 1:n
+    isFault1(commands_index(groupedChangingIndexes1(1,i)) + 1 : commands_index(groupedChangingIndexes1(2,i))) = 1;
+end
+
+
+% FAULT 2
+% Finding the faulty command indexes
+index_negative2 = find(dataArray{1,8} < 0);
+negative2_id = zeros(length(dataArray{1,8}),1);
+% Sets indexes to 1s if the altitude is greater than the given limit
+negative2_id(index_negative2) = 1;
+% And with commands id to find negative values corresponding to COMMANDS 
+faultyCommand2_id = negative2_id & commands_id_only & flight_duration_id &drone_select_id;
+fault2_index = find(faultyCommand2_id==1);
+
+isFault2Command = ismember(commands_index,fault2_index);
+faulty2_id = diff(isFault2Command)~=0;
+changingIndexes2 = find(faulty2_id==1);
+groupedChangingIndexes2 = reshape(changingIndexes2,2,[]);
+% detecting change one index before
+[m,n] = size(groupedChangingIndexes2);
+groupedChangingIndexes2 = ones(m,n) + groupedChangingIndexes2;
+
+isFault2 = zeros(length(dataArray{1,1}),1);
+
+for i = 1:n
+    isFault2(commands_index(groupedChangingIndexes2(1,i)) + 1 : commands_index(groupedChangingIndexes2(2,i))) = 1;
+end
+
 
 %%%%%%%%%  Hello Ewoud %%%%%%%%%%
 % act_id = strcmp(dataArray{1,3},'ROTORCRAFT_CMD');
